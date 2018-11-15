d34ce0c75ff2888a4c235afbc8616df651863e89
Brian Schembri <bschembri@attardco.com>
2018-11-15 16:26:46 +0100

    EyeSelInvDet query returns sum for each SKU

--- src/main/java/com/attardco/entities/EyeSelInvoiceView.java
+++ src/main/java/com/attardco/entities/EyeSelInvoiceView.java
@@ -1,5 +1,6 @@
 package com.attardco.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedNativeQuery;
@@ -14,74 +15,69 @@ import javax.persistence.FieldResult;
         entities = @EntityResult(
                 entityClass = EyeSelInvoiceView.class,
                 fields = {
-                    @FieldResult(name = "SYSREF", column = "SYSREF"),
-                    @FieldResult(name = "SalesOrder", column = "SalesOrder"),
+                	@FieldResult(name = "ROW", column = "ROW"),
                     @FieldResult(name = "ClientCode", column = "ClientCode"),
                     @FieldResult(name = "ItemCode", column = "ItemCode"),
-                    @FieldResult(name = "Qty", column = "Qty"),
-                    @FieldResult(name = "Display", column = "Display"),
+                    @FieldResult(name = "ItemQty", column = "ItemQty"),
                     @FieldResult(name = "InvNum", column = "InvNum")}))
 
 @NamedNativeQuery(name = "getInvoiceDetails",
-		query = "SELECT SoD.SYSREF as SYSREF, SoD.HEADERSYSREF as SalesOrder, SoH.ACCOUNT as ClientCode, Inp.CODE as ItemCode, SoD.BASEQTY * -1 as Qty, SoD.DISPLAY as Display, SoH.REFERENCE as InvNum "
+		query = "SELECT ROW_NUMBER() OVER(ORDER BY SoD.SYSREF) as ROW, SoH.ACCOUNT as ClientCode, Inp.CODE as ItemCode, SUM(SoD.BASEQTY * -1) as ItemQty, SoH.REFERENCE as InvNum  "
 				+ "from EyeSelServer.TEST_FOOD.dbo.SOPOSTHD SoH "
 				+ "inner join EyeSelServer.TEST_FOOD.dbo.SOPOSTDT SoD on SoH.SYSREF = SoD.HEADERSYSREF "
 				+ "inner join EyeSelServer.TEST_FOOD.dbo.INPROFIL InP on SoD.Profilesysref = InP.SYSREF "
-				+ "WHERE (SOh.REFERENCE = :invNum)",
+				+ "WHERE (SOh.REFERENCE = :invNum) "
+				+ "group by SoD.SYSREF, SoH.ACCOUNT, Inp.CODE, SoH.REFERENCE",
 		resultClass = EyeSelInvoiceView.class )
 
 
 public class EyeSelInvoiceView {
 	@Id
-	private java.math.BigDecimal SYSREF;
-	private java.math.BigDecimal SalesOrder;
+	@Column(name = "ROW", updatable = false, nullable = false)
+	private Long ROW;
 	private String ClientCode;
 	private String ItemCode;
-	private Long Qty;
-	private String Display;
+	private Long ItemQty;
 	private java.math.BigDecimal InvNum;
 	
-	public EyeSelInvoiceView() {};
-	
-	public java.math.BigDecimal getSYSREF() {
-		return SYSREF;
+	public EyeSelInvoiceView() {}
+
+	public Long getROW() {
+		return ROW;
 	}
-	public void setSYSREF(java.math.BigDecimal sYSREF) {
-		SYSREF = sYSREF;
+
+	public void setROW(Long rOW) {
+		ROW = rOW;
 	}
-	public java.math.BigDecimal getSalesOrder() {
-		return SalesOrder;
-	}
-	public void setSalesOrder(java.math.BigDecimal salesOrder) {
-		SalesOrder = salesOrder;
-	}
+
 	public String getClientCode() {
 		return ClientCode;
 	}
+
 	public void setClientCode(String clientCode) {
 		ClientCode = clientCode;
 	}
+
 	public String getItemCode() {
 		return ItemCode;
 	}
+
 	public void setItemCode(String itemCode) {
 		ItemCode = itemCode;
 	}
-	public Long getQty() {
-		return Qty;
+
+	public Long getItemQty() {
+		return ItemQty;
 	}
-	public void setQty(Long qty) {
-		Qty = qty;
+
+	public void setItemQty(Long itemQty) {
+		ItemQty = itemQty;
 	}
-	public String getDisplay() {
-		return Display;
-	}
-	public void setDisplay(String display) {
-		Display = display;
-	}
+
 	public java.math.BigDecimal getInvNum() {
 		return InvNum;
 	}
+
 	public void setInvNum(java.math.BigDecimal invNum) {
 		InvNum = invNum;
 	}
